25ab7fbd7fc31999d12f353c37101e1d9438f079
Steven Gill <stevengill97@gmail.com>
2012-05-17 23:48:14 -0700

    added javascript orientation change

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function transform(){
 	document.getElementById("clickbttn").className += ' tfbttn';
 }
+
+var supportsOrientationChange = "onorientationchange" in window,
+    orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+window.addEventListener(orientationEvent, function() {
+    document.getElementById("otest").style.color = "red";
+}, false);
