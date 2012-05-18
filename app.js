280873a56ec0ca64975d80b51e85d3a0d4931811
Steven Gill <stevengill97@gmail.com>
2012-05-18 13:02:36 -0700

    added js pixel ratio detection

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ var supportsOrientationChange = "onorientationchange" in window,
 window.addEventListener(orientationEvent, function() {
     document.getElementById("otest").style.color = "red";
 }, false);
+
+//get the pixel density ratio
+function pixeld(){
+	var dpr = 1;
+	if(window.devicePixelRatio !== undefined) dpr = window.devicePixelRatio;
+	alert(dpr);
+}
