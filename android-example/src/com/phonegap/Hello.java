849ec4f4d4484e956d4d651a41c45710627d5b3f
Dave Johnson <dave.c.johnson@gmail.com>
2012-05-22 06:30:04 +1000

    add resume, pause, url loading override to the plugin

--- android-example/src/com/phonegap/Hello.java
+++ android-example/src/com/phonegap/Hello.java
@@ -5,10 +5,27 @@ import org.apache.cordova.api.Plugin;
 import org.apache.cordova.api.PluginResult;
 
 public class Hello extends Plugin {
+	@Override
     public PluginResult execute(String action, JSONArray args, String callbackId) {
     	if (action.equals("say"))
     		return new PluginResult(PluginResult.Status.OK, "hello world");
     	else 
     		return new PluginResult(PluginResult.Status.ERROR, "wtf mate!");
+    }
+	
+	@Override
+	public void onPause(boolean multitasking) {
+		// save your state!
+	}
+    
+	@Override
+	public void onResume(boolean multitasking) {
+		// retrieve your state!
+	}
+	
+	@Override
+    public boolean onOverrideUrlLoading(String url) {
+    	// do something interesting here :)
+    	return false;
     }
 }
