ef8ae1e25376174ec078bbb2e9b5098b38252809
Dave Johnson <dave.c.johnson@gmail.com>
2012-05-21 14:01:06 +1000

    add cordova lib, xml configs, manifest, and main java class

--- android-example/src/com/phonegap/PhoneGapExampleActivity.java
+++ android-example/src/com/phonegap/PhoneGapExampleActivity.java
@@ -3,11 +3,13 @@ package com.phonegap;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PhoneGapExampleActivity extends Activity {
+import org.apache.cordova.DroidGap;
+
+public class PhoneGapExampleActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
