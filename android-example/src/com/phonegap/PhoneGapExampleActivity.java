af18d68293aa407fbc47aeb9d1bb17828ea0c375
Dave Johnson <dave.c.johnson@gmail.com>
2012-05-21 13:35:58 +1000

    default phonegap android project

---
+++ android-example/src/com/phonegap/PhoneGapExampleActivity.java
@@ -0,0 +1,13 @@
+package com.phonegap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneGapExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
